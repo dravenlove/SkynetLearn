d6e18940b216e6165e59f99545cfb121501c6205
draven <13725162400@163.com>
2023-08-18 17:44:36 +0800

    Shell Add.

---
+++ shell/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export ROOT=..$(cd `dirname $0`; pwd)
+
+cd ../skynet
+make linux
---
+++ shell/ctl.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export ROOT=..$(cd `dirname $0`; pwd)
+export DAEMON = false 
+
+while getopts "DK" arg
+do 
+    case $arg in
+        D)
+            export DAEMON = true
+            ;;
+        K)
+            kill 'cat $ROOT/skynet.pid'
+            exit 0;
+            ;;
+        esac
+done
+
+$ROOT/skynet/skynet $ROOT/config
